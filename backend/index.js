28389248af00b6e68d7017d5d2aa39aa2c688856
Ahmed Baid <baid@gmail.com>
2025-02-16 18:44:50 +0000

    last commit

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
-const cors = require('cors');
-
-const corsOptions = {
-  origin: 'https://full-ecommerce-app-frontend.onrender.com', // Allow this domain
-  credentials: true, // Allow credentials (cookies, etc.)
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}));
 app.use(cookieParser())
 
 // Routes
