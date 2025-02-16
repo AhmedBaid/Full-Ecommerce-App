15d32142ce96d6cdfafcd9cf8723dce127ab75a5
Ahmed Baid <baid@gmail.com>
2025-02-16 18:08:20 +0000

    last commit

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,6 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 const router = require('./routes/index');
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Backend is working!");
-});
 
 
 app.use(express.json({ limit: '50mb' }));
@@ -15,15 +12,15 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL, 
-    credentials: true,
+    origin:process.env.FRONTEND_URL,
+    credentials:true
 }));
 app.use(cookieParser())
 
 // Routes
 app.use("/api", router);
 
-const port = 8080;
+const port =process.env.PORT || 8080;
 connectDB().then(() => {
     app.listen(port, () => {
         console.log("Connected to DB");
--- frontend/src/common/index.jsx
+++ frontend/src/common/index.jsx
@@ -1,4 +1,4 @@
-const backendDomin = "https://full-ecommerce-app.onrender.com"
+const backendDomin = "http://localhost:8080"
 
 const SummaryApi = {
     signUP: {
