7c2c875ff586cfdf6c3dfe7001d46b4f6ae44ccf
Ahmed Baid <baid@gmail.com>
2025-02-16 19:55:54 +0000

    last commit

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,26 @@ const connectDB = require("./config/db");
 const router = require('./routes/index');
 const app = express();
 
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(
     cors({
-      origin: ["http://localhost:3000", "https://full-ecommerce-app-frontend.onrender.com"],
-      credentials: true, 
+        origin: [
+            "http://localhost:3000",
+            "https://full-ecommerce-app-frontend.onrender.com",
+        ],
+        credentials: true,
     })
-  );
+);
 app.use(cookieParser())
 
 // Routes
 app.use("/api", router);
 
-const port =process.env.PORT || 8080;
+const port = 8080;
 connectDB().then(() => {
     app.listen(port, () => {
         console.log("Connected to DB");
